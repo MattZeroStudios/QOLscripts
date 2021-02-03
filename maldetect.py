e0122b328dc1223335386141fe8ea614fa598a48
matthew <matt.zerostudios@gmail.com>
2021-02-02 20:49:55 -0600

    updated names and added some code to qolmalware-detection.py

--- maldetect.py
+++
@@ -1,6 +0,0 @@
-"""
-This script is to scan in the background home directories and quarantine files that are marked as "Malware" By ClamAV
-If a file is marked by ClamAV it will display a pop up letting the user know a file was a hit for malware and has been
-Quarantined. An amazing feature would be to pull ClamAV codes so you can manually quarantine or restore from it via
-another popup window.
-"""
